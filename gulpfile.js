48a6b9228f023ef4c8bd1e13c085dcd91dd8a5a3
Rudik312 <fl.rudik@mail.ru>
2020-12-11 16:06:10 +0700

    added product-slider and product

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,9 @@ function scripts() {
 function slick() {
   return src(["node_modules/normalize.css/normalize.css",
     "node_modules/slick-carousel/slick/slick.css", 
-    "node_modules/magnific-popup/dist/magnific-popup.css"])
+    "node_modules/magnific-popup/dist/magnific-popup.css",
+    "node_modules/rateyo//dist/jquery.rateyo.css"
+  ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
     .pipe(dest("app/css"))
@@ -60,7 +62,10 @@ function slick() {
 function libs() {
   return src([
     "node_modules/slick-carousel/slick/slick.js", 
-    "node_modules/magnific-popup/dist/jquery.magnific-popup.js"])
+    "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+    "node_modules/mixitup/dist/mixitup.js",
+    "node_modules/rateyo/dist/jquery.rateyo.js"
+  ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
     .pipe(dest("app/js"))
